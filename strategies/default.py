9d7a1957aed6d803c1c47e327d7cbf82570ac4d0
Arsen Nuramatov <x@ctrl.sn>
2025-09-04 17:08:30 +0200

    write stubs, almost done

--- strategies/default.py
+++ strategies/default.py
@@ -17,62 +17,77 @@ class Strategy(BaseStrategy):
     def _decide(self, attrs: list[str]) -> bool:
         self.log()
 
-        # XXX obvious cases ----------------------------------------------------
+        # obvious cases --------------------------------------------------------
 
         unmet_quotas: list[str] = [k for k, v in self.deficits.items() if v > 0]
         if not unmet_quotas:
             # atp we just need to get 1k in total
-            return self._accept("we just need more people", attrs)
+            return self._accept(
+                attrs,
+                reason="we just need more people",
+            )
 
         contributes_to_deficit = any(k in unmet_quotas for k in attrs)
         if not contributes_to_deficit:
             # person is useless, safe to reject
-            return self._reject("doesnt decrease deficits")
+            return self._reject(reason="doesnt decrease deficits")
 
         has_all_attributes = len(attrs) == len(self.deficits)
         if has_all_attributes:
-            return self._accept("all attributes", attrs)
-
-        # XXX main logic -------------------------------------------------------
-
-        # generally a good idea: think about constraint satisfaction probas
-        # FUCK simplified idea: probability that there will be `deficit[k]` people with attribute `k` out of the next `remaining_budget + remaining_places` people
-        remaining = (
-            max(0, self.remaining_budget) + self.remaining_places
-        )  # FUCK idk if this makes sense
-        satisfaction_probas = {  # TODO actually "satisfiability", rename
-            k: binom.logsf(v, remaining, self.relative_frequencies[k])
-            for k, v in self.deficits.items()
-        }  # FUCK might be v-1 actually, check
-        print(
-            "---", self.remaining_budget, self.deficits, satisfaction_probas
-        )  # TODO move or remove?
-        total_proba = sum(
-            satisfaction_probas.values()
-        )  # FUCK we're basically assuming here rejects are equiv to accepts; feels ok
-        # FUCK ideally we would calculate reject vs accept, but we can't without a joint, so compare accept to average case or smth; feels ok
-        accept_probas = {
-            k: binom.logsf(
-                v - (1 if k in attrs else 0),
-                remaining - 1,
-                self.relative_frequencies[k],
+            return self._accept(
+                attrs,
+                reason="all attributes",
             )
-            for k, v in self.deficits.items()
-        }
-        proba_diff = sum(accept_probas.values()) - total_proba
-        if proba_diff < 0:
-            return self._reject("decreases succ proba")
-        return self._accept("doesn't decrease succ proba", attrs)
 
-    def _reject(self, reason: str) -> bool:
-        print(reason)
+        # main logic -----------------------------------------------------------
+
+        if self._satisfiability_logproba_diff() < 0:
+            return self._reject(reason="proba")
+        return self._accept(
+            attrs,
+            reason="proba",
+        )
+
+    def _satisfiability_logproba_diff(self) -> float:
+        # TODO
+        deficits_if_accept = ...
+        deficits_if_reject = ...
+        logproba_diff = self._satisfiability_logproba(
+            deficits_if_accept
+        ) - self._satisfiability_logproba(deficits_if_reject)
+        return logproba_diff
+
+    def _satisfiability_logproba(self, deficits) -> float:
+        """
+        calculate probability that there is a subset of `remaining_places` people in the next `remaining_budget + remaining_places` people
+        s.t. when they are accepted, the deficits are non-positive;
+        in other words, proba that it's possible to win
+        """
+
+        # logprobas = sum(
+        #     binom.logsf(deficit - 1, remaining, proba)
+        # )
+
+        return 0.0
+
+    def _joint(self):
+        pass
+
+    def _reject(self, reason: str = "") -> bool:
+        if reason:
+            print(reason)
         self.remaining_budget -= 1
         if self.remaining_budget == 0:
             print("--- rejection budget exceeded ---")
         return False
 
-    def _accept(self, reason: str, attrs: list[str]) -> bool:
-        print(reason)
+    def _accept(
+        self,
+        attrs: list[str],
+        reason: str = "",
+    ) -> bool:
+        if reason:
+            print(reason)
         self.remaining_places -= 1
         for k in attrs:
             if k in self.deficits and self.deficits[k] > 0:
